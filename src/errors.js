4ad4fca9d6c889f2c8ece1cebe3b1d4aa6cc4975
Brandon Aaron <hello.brandon@aaron.sh>
2019-11-20 18:03:53 -0500

    Add names for error objects

--- src/errors.js
+++ src/errors.js
@@ -1,5 +1,13 @@
-class MissingOutputFormatError extends Error {}
-class InvalidOutputFormatError extends Error {}
+class MissingOutputFormatError extends Error {
+  get name () {
+    return 'MissingOutputFormatError'
+  }
+}
+class InvalidOutputFormatError extends Error {
+  get name () {
+    return 'InvalidOutputFormatError'
+  }
+}
 
 module.exports = {
   MissingOutputFormatError,
